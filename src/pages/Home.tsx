a704250ac8180886111bf260a1ec94f80bb35b1a
emrahugurlu2003 <135663130+emrahugurlu2003@users.noreply.github.com>
2023-09-01 16:03:24 +0300

    getTodos method using axios

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+interface ITodoType {
+  todoText: string;
+  isDone: boolean;
+  id: string | number;
+}
 
 const Home = () => {
-  return <div>Home</div>;
-};
+  //!useState<ITodoType[]> ensures that todos is of type ITodoType[]
+  //from the start, providing better type safety in the component.
+  const [todos, setTodos] = useState<ITodoType[]>([]);
 
+  //calls getTodos method onPageLoad
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  //!For fetching data from API
+  //!Reading environment variables from ".env.local"
+  const ENDPOINT_URL = import.meta.env.VITE_APP_BASE_URL;
+  const getTodos = async () => {
+    //const responseData = await axios(ENDPOINT_URL);
+    //{data: Array(3), status: 200, statusText: 'OK', headers: AxiosHeaders, config: {…}, …}
+    //const { data } = responseData;
+    //Destructuring data from the responseData
+    const { data } = await axios(ENDPOINT_URL);
+    return <div>Home</div>;
+  };
+};
 export default Home;
