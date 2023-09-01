e846d27fcdbbc1cf72a0d53bff141e0f533fb8e5
emrahugurlu2003 <135663130+emrahugurlu2003@users.noreply.github.com>
2023-09-01 16:28:31 +0300

    getTodos method fixed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Container, Typography } from "@mui/material";
 interface ITodoType {
   todoText: string;
   isDone: boolean;
@@ -18,14 +19,26 @@ const Home = () => {
 
   //!For fetching data from API
   //!Reading environment variables from ".env.local"
-  const ENDPOINT_URL = import.meta.env.VITE_APP_BASE_URL;
+  const ENDPOINT_URL: string = import.meta.env.VITE_APP_BASE_URL;
   const getTodos = async () => {
     //const responseData = await axios(ENDPOINT_URL);
     //{data: Array(3), status: 200, statusText: 'OK', headers: AxiosHeaders, config: {…}, …}
     //const { data } = responseData;
     //Destructuring data from the responseData
     const { data } = await axios(ENDPOINT_URL);
-    return <div>Home</div>;
   };
+  return (
+    <Container>
+      <Typography
+        color="error"
+        align="center"
+        variant="h2"
+        component={"h1"}
+        mt={3}
+      >
+        Todo App With Typescript
+      </Typography>
+    </Container>
+  );
 };
 export default Home;
