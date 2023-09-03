d63cc1a046558dd24b2e4f2600ec3a4ce605e7aa
emrahugurlu2003 <135663130+emrahugurlu2003@users.noreply.github.com>
2023-09-03 11:54:10 +0300

    TodoListItem and InerfaceTodoListItem is completed

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import * as React from "react";
 
-const TodoListItem = () => {
-  return <div>TodoListItem</div>;
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import FolderIcon from "@mui/icons-material/Folder";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const TodoListItem = ({ customColor, items }: InterfaceTodoListItemProps) => {
+  return (
+    <ListItem
+      disableGutters
+      sx={{ cursor: "pointer", overflow: "hidden" }}
+      secondaryAction={
+        <IconButton
+          aria-label="comment"
+          sx={{ color: customColor, "&:hover": { color: "red" } }}
+          //onClick={() => deleteTodo(todo.id)}
+        >
+          //!If the List is empty, the DeleteIcon is not rendered
+          {items.length > 0 && <DeleteIcon />}
+        </IconButton>
+      }
+    >
+      <ListItemText
+        //primary={todo.todo}
+        //onClick={() => toggleTodo(todo)}
+        sx={{ wordWrap: "break-word" }}
+      />
+    </ListItem>
+  );
 };
 
 export default TodoListItem;
