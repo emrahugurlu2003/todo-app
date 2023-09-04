a4e9d8b1639a86a914af33b6ba277c4cafbd1438
emrahugurlu2003 <135663130+emrahugurlu2003@users.noreply.github.com>
2023-09-04 19:12:45 +0300

    All Unused imports are removed

--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -1,15 +1,9 @@
 import * as React from "react";
 
-import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
-import FolderIcon from "@mui/icons-material/Folder";
 import DeleteIcon from "@mui/icons-material/Delete";
-import AutoDeleteIcon from "@mui/icons-material/AutoDelete";
 import PendingIcon from "@mui/icons-material/Pending";
 
 const TodoListItem = ({
